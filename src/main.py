155e31e64dae5e31923c3563a92ed2af26d0a6f0
Nightfall3594 <nightfall3594@gmail.com>
2025-10-01 19:50:50 +0800

    feat: add initial routes

--- src/main.py
+++ src/main.py
@@ -2,7 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def index():
-    return {'message': 'Hello World'}
 
+@app.get('/ping')
+def ping():
+    return {'ping': 'pong'}  # note: for testing connectivity only
+
+@app.get('/timeline')
+async def timeline():
+    return {'timeline': []}  # return a timeline. Note: timeline items only
+
+
+@app.get('/articles/{article_slug}')
+async def article(article_slug: str):
+    return {'id': ""}  # return an Article object using the slug. Note: Body is included
+
+
+@app.get('/articles')
+async def articles():
+    return {'articles': []}  # get all articles, exclude body
+
+
+@app.get('/thoughts')
+async def thoughts():
+    return {'thoughts': []}  # get all thought items
