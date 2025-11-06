cccf2676a41a66102985dd54ac68d9023ffa0173
Nightfall3594 <nightfall3594@gmail.com>
2025-11-06 15:04:24 +0800

    fix: fix issue with dependency injection

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,12 +1,9 @@
 from fastapi import Depends
 
 from src.db_config import SessionLocal
-from src.repos import *
 from src.services import *
 from src.repos import *
 
-
-# TODO: Replace Depends() with generator expression
 
 def get_db():
     with SessionLocal() as session:
@@ -14,21 +11,21 @@ def get_db():
         session.commit()
 
 # callbacks for dependency injection
-def get_article_service():
-    return ArticleService(PostgresArticleRepository(next(get_db())))
+def get_article_service(db = Depends(get_db)):
+    return ArticleService(PostgresArticleRepository(db))
 
 
-def get_project_service():
-    return ProjectService(PostgresProjectRepository(next(get_db())))
+def get_project_service(db = Depends(get_db)):
+    return ProjectService(PostgresProjectRepository(db))
 
 
-def get_thought_service():
-    return ThoughtService(PostgresThoughtRepository(next(get_db())))
+def get_thought_service(db = Depends(get_db)):
+    return ThoughtService(PostgresThoughtRepository(db))
 
 
-def get_timeline_service():
+def get_timeline_service(db = Depends(get_db)):
     return TimelineService(
-        articles = PostgresArticleRepository(next(get_db())),
-        projects = PostgresProjectRepository(next(get_db())),
-        thoughts = PostgresThoughtRepository(next(get_db())),
+        articles = PostgresArticleRepository(db),
+        projects = PostgresProjectRepository(db),
+        thoughts = PostgresThoughtRepository(db),
     )
